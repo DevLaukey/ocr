85e692b5dd3d824aba0d002bf61bf5d0ce60d3fc
DevLaukey <63729244+DevLaukey@users.noreply.github.com>
2024-01-30 00:35:49 +0300

    To check for alternatives

--- .history/index_20240130000117.js
+++ .history/index_20240130000117.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const app = express();
 const optiic = new (require("optiic"))({
-  apiKey:  "api_test_key", // Not required, but having one removes limits (get your key at https://optiic.dev).
+    apiKey: "7hagb6RKxJECkWiESUQHmUkFVMXvAoTvUxiTyACA718n"
+        "api_test_key", // Not required, but having one removes limits (get your key at https://optiic.dev).
 });
 
 app.use(express.json());
 
 app.post("/", (req, res) => {
-  console.log("object");
-  const img = req.body.url;
+    console.log(req.body)
+    const img = req.body.url;
   let options = {
     image: img, // url of the image
     mode: "ocr", // ocr
   };
-    optiic.process(options).then((result) => {
-      console.log(result);
+  optiic.process(options).then((result) => {
     res.send(result ? result.text : "This image doesn't contain any text!");
-  }).catch((err) => {
-    console.log(err);
   });
 });
 
--- .history/index_20240130001356.js
+++ .history/index_20240130001356.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const optiic = new (require("optiic"))({
-  apiKey:  "api_test_key", // Not required, but having one removes limits (get your key at https://optiic.dev).
+  apiKey: "7hagb6RKxJECkWiESUQHmUkFVMXvAoTvUxiTyACA718n",
+  // "api_test_key", // Not required, but having one removes limits (get your key at https://optiic.dev).
 });
 
 app.use(express.json());
@@ -13,11 +14,8 @@ app.post("/", (req, res) => {
     image: img, // url of the image
     mode: "ocr", // ocr
   };
-    optiic.process(options).then((result) => {
-      console.log(result);
+  optiic.process(options).then((result) => {
     res.send(result ? result.text : "This image doesn't contain any text!");
-  }).catch((err) => {
-    console.log(err);
   });
 });
 
